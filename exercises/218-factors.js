215c8bf8b6eaa1c623fd2410ac6d3d917fa56cc2
Taylor Blocker <taylorblocker@gmail.com>
2019-07-06 22:43:24 -0500

    Started the factors exercise. Looking to find a way to simplify the process of what I have. I found some very short examples online but I want to try and figure out a way to do it myself if possible.

--- exercises/218-factors.js
+++ exercises/218-factors.js
@@ -6,7 +6,47 @@
 // gcd(5, 1) --> 1
 // gcd(3, 15) --> 3
 // gcd(50, 20) --> 10
+function gcd(num1, num2){
+    let numArray1 = [];
+    let numArray2 = [];
+    let finalArray = [];
+    let longestArray;
+    for (let i = 1; i < Math.floor(num1); i++) {
+      if (num1 % i === 0) {
+        numArray1.push(i);
+        }  
+    }
+      for (let i = 1; i < Math.floor(num2); i++) {
+        if (num2 % i === 0) {
+        numArray2.push(i);
+        }  
+    } 
+      for (let i = 0; i < numArray1.length || numArray2.length; i++) {
+        if (numArray1.lenth > numArray2.length){
+            longestArray = numArray1;
+        }else {
+            longestArray = numArray2;
+        }
+        if (numArray1[i] === numArray2[i]) {
+          finalArray.push(longestArray[i]);
+      }
+      return finalArray;
+  }
+}
 
+    
+    
+//     );(num1 % num2 !== 0){
+//         return 1;
+//     }
+//     else {
+//         return num1 / num2
+
+
+//     }
+
+
+// }
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

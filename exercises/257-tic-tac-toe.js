5555533b0d7b8e330d9be4afd632d6bdc268d7a5
Taylor Blocker <taylorblocker@gmail.com>
2019-07-14 18:01:49 -0500

    completed the257 exercise, I belive there is a way to tighten this up with a for loop. Maybe using i to iterate through each possition in the array an then compare the elements. maybe create 2 for loops for x and o?'

--- exercises/257-tic-tac-toe.js
+++ exercises/257-tic-tac-toe.js
@@ -23,3 +23,90 @@
 //   [null, 'X', 'X']
 //   ])
 // null
+function ticTacToe(array){
+    let winner;
+        if(array[0][0] === 'O' &&
+           array[0][1] === 'O' &&
+           array[0][2] === 'O'){
+            winner = 'O';
+        }
+        else if(array[1][0] === 'O' &&
+                array[1][1] === 'O' &&
+                array[1][2] === 'O'){
+            winner = 'O';
+        }
+        else if(array[2][0] === 'O' &&
+                array[2][1] === 'O' &&
+                array[2][2] === 'O'){
+            winner = 'O';
+        }
+        else if(array[0][0] === 'O' &&
+                array[1][0] === 'O' &&
+                array[2][0] === 'O') {
+            winner = 'O'
+        }
+        else if(array[0][1] === 'O' &&
+                array[1][1] === 'O' &&
+                array[2][1] === 'O') {
+            winner = 'O'
+        }
+        else if(array[0][2] === 'O' &&
+                array[1][2] === 'O' &&
+                array[2][2] === 'O') {
+            winner = 'O'
+        }
+        else if(array[0][0] === 'O' &&
+                array[1][1] === 'O' &&
+                array[2][2] === 'O') {
+            winner = 'O'
+        } 
+        else if(array[0][2] === 'O' &&
+                array[1][1] === 'O' &&
+                array[2][0] === 'O') {
+            winner = 'O'
+        }
+        else if(array[0][0] === 'X' &&
+                array[0][1] === 'X' &&
+                array[0][2] === 'X'){
+            winner = 'X';
+        }
+        else if(array[1][0] === 'X' &&
+                array[1][1] === 'X' &&
+                array[1][2] === 'X'){
+            winner = 'X';
+        }
+        else if(array[2][0] === 'X' &&
+                array[2][1] === 'X' &&
+                array[2][2] === 'X'){
+            winner = 'X';
+        }
+        else if(array[0][0] === 'X' &&
+                array[1][0] === 'X' &&
+                array[2][0] === 'X') {
+            winner = 'X'
+        }
+        else if(array[0][1] === 'X' &&
+                array[1][1] === 'X' &&
+                array[2][1] === 'X') {
+            winner = 'X'
+        }
+        else if(array[0][2] === 'X' &&
+                array[1][2] === 'X' &&
+                array[2][2] === 'X') {
+            winner = 'X'
+        }
+        else if(array[0][0] === 'X' &&
+                array[1][1] === 'X' &&
+                array[2][2] === 'X') {
+            winner = 'X'
+        } 
+        else if(array[0][2] === 'X' &&
+                array[1][1] === 'X' &&
+                array[2][0] === 'X') {
+            winner = 'X'
+        }
+        else {
+            winner = null
+                }
+            return winner
+            }
